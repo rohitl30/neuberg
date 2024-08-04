9e6463b9fb03c2b2f53c8a78a2f992fbd91ca36a
rohitl30 <rohit.luniya619@gmail.com>
2024-08-04 19:05:33 +0530

    Add orange textbox

--- src/components/FullBodyPackage/FullBodyPackage.js
+++ src/components/FullBodyPackage/FullBodyPackage.js
@@ -6,7 +6,12 @@ const FullBodyPackage = () => {
   return (
     <div className='fullbodypackage'>
     <h1><span className='violet-text'> FULL BODY </span><span className='orange-text'> HEALTH PACKAGE </span></h1>
-
+    <div className='orange-textbox'>
+            <div> PRICE </div>
+            <div className='price-text'> <br />
+            <span className='price-large'>&#8377; 999</span>
+            </div>
+          </div>
     </div>
 
   );
